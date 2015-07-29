b6c55ca79407c072febf7bf8e3faf874db4e0a46
Phillip Curl <phillipcurl@gmail.com>
2015-07-28 23:04:47 -0400

    Undoing a boneheaded move

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'port': process.env.PORT || 8888,
-	'database': 'mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o',
-	'secret': 'ilovescotchscotchyscotchscotch',
-	'kimonoKey': 'DbCxO9Kf0d5N7OcWsQJ5zyvnuGwGA8Dd',
-	'kimonoToken': 'SkI2CTI8xakCOHlmRQhUSBXc1J9H0Rov'
-};
